595afbd6ea6e2763b243d6cc94a65af54bfa8bc0
konrad-ch <konrad-ch@users.noreply.github.com>
2020-08-22 20:54:52 +0200

    payment methods enum update

--- src/Types/Enums/PaymentMethod.php
+++ src/Types/Enums/PaymentMethod.php
@@ -11,4 +11,25 @@ class PaymentMethod {
     const MOBILE = "mobile";
     const PAYPAL = "paypal_gateway";
     const SKRILL = "skrill";
+    const SKRILL_NETELLER = "skrill_neteller";
+    const DINERS_CLUB_INTERNATIONAL = "boacompra_diners";
+    const AMERICAN_EXPRESS = "boacompra_amex";
+    const REDCOMPA = "boacompra_redcompra";
+    const RIPLEY = "boacompra_ripley";
+    const CENCOSUD = "boacompra_cencosud";
+    const MAGNA = "boacompra_magna";
+    const CMR = "boacompra_cmr";
+    const PARIS = "boacompra_paris";
+    const JUMBO = "boacompra_jumbo";
+    const EASY = "boacompra_easy";
+    const CULTURE_VOUCHER = "payletter_culture_voucher";
+    const SMART_CULTURE_VOUCHER = "payletter_smart_culture_voucher";
+    const BOOK_VOUCHER = "payletter_book_voucher";
+    const TMONEY = "payletter_tmoney";
+    const CASHBEE = "payletter_cashbee";
+    const TEENCASH = "payletter_teencash";
+    const EGGMONEY = "payletter_eggmoney";
+    const HAPPYMONEY = "payletter_happymoney";
+    const ONCASH = "payletter_oncash";
+    const BLIK = "paymento_blik";
 }
