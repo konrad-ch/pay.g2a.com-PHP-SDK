300e6234d1bc207493911bc9217546d1ce3b6544
konrad-ch <konrad-ch@users.noreply.github.com>
2020-08-23 10:42:02 +0200

    payment methods enum update

--- src/Types/Enums/PaymentMethod.php
+++ src/Types/Enums/PaymentMethod.php
@@ -33,4 +33,22 @@ class PaymentMethod {
     const HAPPYMONEY = "payletter_happymoney";
     const ONCASH = "payletter_oncash";
     const BLIK = "paymento_blik";
+    const MTRANSFER = "paymento_mtransfer";
+    const IPKO = "paymento_ipko";
+    const ING = "paymento_ing";
+    const PEKAO24 = "paymento_pekao24";
+    const BZWBK = "paymento_bzwbk";
+    const MILLENIUM = "paymento_millenium";
+    const ALIOR = "paymento_alior";
+    const CREDIT_AGRICOLE = "paymento_creditagricole";
+    const BNP_PARIBAS = "paymento_bnpparibas";
+    const GETIN_BANK = "paymento_getin";
+    const INTELIGO = "paymento_inteligo";
+    const BANK_POCZTOWY = "paymento_pocztowy";
+    const TMOBILE_BANK = "paymento_tmobile";
+    const CITI_HANDLOWY = "paymento_citi";
+    const ENVELO = "paymento_envelo";
+    const IDEA_BANK = "paymento_ideabank";
+    const PLUS_BANK = "paymento_plusbank";
+    const NOBLE_BANK = "paymento_noble";
 }
